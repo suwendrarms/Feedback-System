c25385815ff95f146ad51581232cd173f46d626f
suwendra-ascension <120699820+suwendra-ascension@users.noreply.github.com>
2025-07-26 21:41:32 +0530

    first commit

---
+++ app/Views/auth/user-logs.php
@@ -0,0 +1,49 @@
+<!-- load main layout with datatable -->
+<?= $this->extend('auth/layouts/default-table') ?>
+
+<!-- load main content -->
+<?= $this->section('main') ?>
+
+    <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-3">
+        <h1 class="h2">Login Logs</h1>
+    </div>
+
+    <div class="card p-3">
+        <div class="table-responsive">
+            <table width="100%" class="table table-hover" id="dataTables-table1" data-order='[[ 0, "desc" ]]'>
+                <thead>
+                    <tr>
+                        <th>Date</th>
+                        <th>Time</th>
+                        <th>Name</th>
+                        <th>IP Address</th>
+                        <!-- <th>Location</th> -->
+                        <th>Browser</th>
+                        <th>Status</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php foreach ($data as $log):?>
+                        <tr>
+                            <td><?= $log['date'] ?></td>
+                            <td><?= $log['time'] ?></td>
+                            <td><?= $log['name'] ?></td>
+                            <td><?= $log['ip'] ?></td>
+                            <!-- <td>Philippines</td> -->
+                            <td><?= $log['browser'] ?></td>
+                            <td><?= $log['status'] ?></td>
+                        </tr>
+                    <?php endforeach;?>
+                </tbody>
+            </table>
+        </div>
+    </div>
+
+<?= $this->endSection() ?>
+
+<?= $this->section('script') ?>
+    <script>
+         
+        $('#dataTables-table1').DataTable({responsive: true,pageLength: 15,lengthChange: false,searching: true,ordering: true});
+    </script>
+<?= $this->endSection() ?>
